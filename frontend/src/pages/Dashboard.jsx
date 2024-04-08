a6873dd5b5251cf10c76c872719f4126e46cef6b
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-04-08 18:21:23 +0530

    again conflict resolve

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,11 +4,8 @@ import { Sidebar } from "@/components/Sidebar"
 export const Dashboard = () => {
   return <div>
     <WorkerBar/>
-<<<<<<< HEAD
     <div style={{display:"flex"}}>
     <Sidebar/><Feed/>
     </div>
-=======
->>>>>>> c1d9413346742e7c1c21dc1f83025be0d8a9cfd4
-  </div>
+    </div>
 }
