65fdb1df44b24a677b68c7fe968e7f80b57cf5f9
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-04-10 23:46:12 +0530

    added token features

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Appbar } from "../components/AppBar";
 import { useNavigate } from "react-router-dom";
 
@@ -8,25 +9,36 @@ export const Home = () => {
         navigate(signin);
     }
     const toChoose = () => {
-      let choose = "/choose";
-      navigate(choose);
+        let choose = "/choose";
+        navigate(choose);
     }
 
+    const token = localStorage.getItem("token")
+    const navigatedashboard = () => {
+        console.log(token)
+        if(token){
+            navigate("/dashboard")
+        }
+    }
+
+    useEffect(()=>{navigatedashboard()},[])
+   
+
     return <div>
-       <div className="flex justify-between h-16">
-        <div className="flex justify-between items-center space-x-5">
-            <div className="font-league-spartan">jobjolt</div>    
-            <button className="item-center pl-3 pr-3 ">Hire</button>
-            <button className="item-center pl-3 pr-3 ">Work</button>
-            <button className="item-center pl-3 pr-3">Why JobJolt</button>
-        </div>
-        <div className="flex items-center pr-5">
-            <div> 
-                <button onClick={toSignin} className="item-center pl-3 pr-3 "> Log in</button>
-                <button onClick={toChoose} className="item-center pl-3 pr-3 "> Sign up</button>
+        <div className="flex justify-between h-16">
+            <div className="flex justify-between items-center space-x-5">
+                <div className="font-league-spartan">jobjolt</div>
+                <button className="item-center pl-3 pr-3 ">Hire</button>
+                <button className="item-center pl-3 pr-3 ">Work</button>
+                <button className="item-center pl-3 pr-3">Why JobJolt</button>
+            </div>
+            <div className="flex items-center pr-5">
+                <div>
+                    <button onClick={toSignin} className="item-center pl-3 pr-3 "> Log in</button>
+                    <button onClick={toChoose} className="item-center pl-3 pr-3 "> Sign up</button>
+                </div>
             </div>
         </div>
-    </div>
         hello
     </div>
 }
