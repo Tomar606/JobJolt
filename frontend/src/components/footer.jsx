1c18db247d47cf5e5b5eeade01465cbf258f5c04
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-04-13 00:53:33 +0530

    made buttons clickable

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -5,91 +5,92 @@ const Footer = () => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-gray-800 text-white">
       <div className="space-y-2">
         <div className="font-bold text-lg">ABOUT</div>
-        <div>Contact us</div>
-        <div>About us</div>
-        <div>Report Infringement</div>
-        <div>Privacy Policy</div>
-        <div>Terms of Use</div>
-        <div>Copyright</div>
+        <a href="#" className="block hover:underline">Contact us</a>
+        <a href="#" className="block hover:underline">About us</a>
+        <a href="#" className="block hover:underline">Report Infringement</a>
+        <a href="#" className="block hover:underline">Privacy Policy</a>
+        <a href="#" className="block hover:underline">Terms of Use</a>
+        <a href="#" className="block hover:underline">Copyright</a>
       </div>
 
       <div className="space-y-2">
         <div className="font-bold text-lg">CONNECT US</div>
-        <div className="flex items-center space-x-2">
+        <a href="mailto:theshoppiemail@gmail.com" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png"
             height="30px"
             width="30px"
             alt="Gmail icon"
           />
-          <span>mail@jobjolt.com</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>theshoppiemail@gmail.com</span>
+        </a>
+        <a href="https://www.instagram.com/theshoppinghomiez" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png"
             height="30px"
             width="30px"
             alt="Instagram icon"
           />
-          <span>@jobjolt</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>@theshoppinghomiez</span>
+        </a>
+        <a href="https://www.facebook.com/theshoppinghomiez" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png"
             height="30px"
             width="30px"
             alt="Facebook icon"
           />
-          <span>@jobjolt</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>@theshoppinghomiez</span>
+        </a>
+        <a href="https://twitter.com/theshoppinghomiez" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png"
             height="30px"
             width="30px"
             alt="Twitter icon"
           />
-          <span>@jobjolt</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>@theshoppinghomiez</span>
+        </a>
+        <a href="https://www.youtube.com/theshoppinghomiez" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"
             height="26px"
             width="32px"
             alt="YouTube icon"
           />
-          <span>@jobjolt</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>@theshoppinghomiez</span>
+        </a>
+        <a href="https://www.reddit.com/r/theshoppinghomiez" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://freelogopng.com/images/all_img/1658834095reddit-logo-png.png"
             height="30px"
             width="30px"
             alt="Reddit icon"
           />
-          <span>r/jobjolt</span>
-        </div>
-        <div className="flex items-center space-x-2">
+          <span>r/theshoppinghomiez</span>
+        </a>
+        <a href="https://github.com/theshoppinghomies" className="flex items-center space-x-2 hover:underline">
           <img
             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
             height="30px"
             width="30px"
             alt="GitHub icon"
           />
-          <span>github.com/pranshu0604/jobjolt</span>
-        </div>
+          <span>github.com/theshoppinghomies</span>
+        </a>
       </div>
 
       <div className="space-y-2">
         <div className="font-bold text-lg">Registered Office Address</div>
-        <div>Flat Number 301, Madhu Chandan Apartments</div>
-        <div>Vallabh Nagar, Indore</div>
-        <div>Pin Code: 452003</div>
-        <div>Madhya Pradesh, India</div>
+        <a href="#" className="block hover:underline">Flat number 301, Madhu Chandan Apartments</a>
+        <a href="#" className="block hover:underline">Vallabh Nagar, Indore</a>
+        <a href="#" className="block hover:underline">Pin Code: 452003</a>
+        <a href="#" className="block hover:underline">Madhya Pradesh, India</a>
       </div>
     </div>
   );
 };
 
 export default Footer;
+
 
