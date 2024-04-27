948daa7d96c52b5b9de4a501c51035a953659d00
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-04-27 13:48:45 +0530

    new cover

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                     <button onClick={toProfile} className="py-2 px-4 rounded-full text-sm font-medium text-white hover:bg-gray-200">
                         Visit Profile
                     </button>
-                    <button onClick={logout} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                    <button onClick={logout} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                         Logout
                     </button>
                 </>
@@ -42,10 +42,10 @@ const Navbar = () => {
         } else if (htoken) {
             return (
                 <>
-                    <button onClick={() => window.alert("Hirer profile page is still in the making")} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                    <button onClick={() => window.alert("Hirer profile page is still in the making")} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                         Visit Profile
                     </button>
-                    <button onClick={logout} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                    <button onClick={logout} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                         Logout
                     </button>
                 </>
@@ -53,10 +53,10 @@ const Navbar = () => {
         } else {
             return (
                 <>
-                    <button onClick={toSignin} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                    <button onClick={toSignin} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                         Log in
                     </button>
-                    <button onClick={toChoose} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                    <button onClick={toChoose} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                         Sign up
                     </button>
                 </>
@@ -69,21 +69,21 @@ const Navbar = () => {
     };
 
     return (
-        <div className="flex justify-between items-center h-16 bg-white shadow-md px-5">
+        <div className="flex justify-between items-center h-16 bg-black shadow-md px-5">
             <div className="flex items-center space-x-6">
-                <button onClick={() => navigate("/")} className="font-bold text-xl text-gray-800">
+                <button onClick={() => navigate("/")} className="font-bold text-xl text-white">
                     JobJolt v1.0
                 </button>
-                <button className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                <button className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                     Hire
                 </button>
-                <button className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                <button className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                     Work
                 </button>
-                <button onClick={toAboutUs} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                <button onClick={toAboutUs} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                     About Us
                 </button>
-                <button onClick={toWhyPage} className="py-2 px-4 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
+                <button onClick={toWhyPage} className="py-2 px-4 rounded-lg text-sm font-medium text-white hover:bg-gray-200">
                     Why JobJolt
                 </button>
             </div>
